ef7c567fc0fb301214332b8e0fb881686e69f896
IanDarwin <>
2006-09-23 02:45:01 +0000

    Mock examples with JMock.

---
+++ src/mocking/TestJButtonWithJMock.java
@@ -0,0 +1,36 @@
+package mocking;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+
+/**
+ * Test the JButton to be sure it calls the mock ActionListener correctly
+ */
+public class TestJButtonWithJMock extends MockObjectTestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testJButtonListenerGetsNotified() {
+
+		// set up
+		Mock mockListener = mock(ActionListener.class);
+		JButton eventSource = new JButton("Hello");
+		eventSource.addActionListener((ActionListener) mockListener.proxy());
+
+		// expectations
+		mockListener.expects(once()).method("actionPerformed").with(isA(ActionEvent.class));
+
+		// execute
+		eventSource.doClick();
+
+		// Uncomment this to watch it fail
+		// eventSource.doClick();
+	}
+}
---
+++ src/mocking/TestPublisherWithJMock.java
@@ -0,0 +1,73 @@
+package mocking;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+
+/**
+ * Demonstrate use of JMock to test the Publisher of a pub/sub
+ * communications package, using a mock Subscriber.
+ * @author Adapted from a tutorial on the JMock web site.
+ */
+public class TestPublisherWithJMock extends MockObjectTestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/** The general contract of a Subscriber */
+	interface Subscriber {
+		public void receive(String message);
+	}
+	/** The general contract of a Publisher */
+	interface Publisher {
+		public void add(Subscriber sub);
+		public void publish(String message);
+	}
+
+	public void testOneSubscriberReceivesAMessage() {
+
+		// set up
+		Mock mockSubscriber = mock(Subscriber.class);
+		Publisher publisher = new PublisherImpl();
+		publisher.add((Subscriber) mockSubscriber.proxy());
+
+		String message = "message";
+
+		// expectations
+		mockSubscriber.expects(once()).method("receive").with( eq(message));
+
+		// To watch it fail, uncomment this line:
+		// message = "mensaje";
+
+		// execute
+		publisher.publish(message);
+	}
+
+	class PublisherImpl implements Publisher {
+
+		List<String> messages = new ArrayList<String>();
+		Set<Subscriber> subscribers = new HashSet<Subscriber>();
+
+		public void add(Subscriber sub) {
+			// Might need to send any previous messages...
+
+			// Add them to the list
+			subscribers.add(sub);
+		}
+
+		public void publish(String message) {
+			// Add message to list of messages
+			messages.add(message);
+			for (Subscriber sub : subscribers) {
+				System.out.println("Sending to " + sub);
+				sub.receive(message);
+			}
+		}
+
+	}
+}
