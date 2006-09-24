e9a9624fd087d7d1393a2d6d22e53262240c1141
IanDarwin <>
2006-09-24 23:54:57 +0000

    Put in both tearDown() methods *without* their annotations, to show that the method name is not special in JUnit 4.

--- src/intro/FooTest.java
+++ src/intro/FooTest.java
@@ -12,15 +12,32 @@ public class FooTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		System.out.println("FooTest.setUpBeforeClass()");
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("FooTest.setUp()");
 	}
 
 	@Test public void test1() throws Exception {
-			Foo foo = new Foo();
-			int value = foo.getValue();
-			assertEquals(42, value);
+		System.out.println("FooTest.test1()");
+		Foo foo = new Foo();
+		int value = foo.getValue();
+		assertEquals(42, value);
+	}
+
+	/** This is missing its @After tag deliberately, to show
+	 * that with out it, *this method does not get called in JUnit 4*
+	 */
+	public void tearDown() {
+		System.out.println("FooTest.tearDown()");
+	}
+
+	/** This is missing its @After tag deliberately, to show
+	 * that with out it, *this method does not get called in JUnit 4*
+	 */
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("FooTest.tearDownAfterClass()");
 	}
 }
