09cfe03ec3dbbd944077e9da78052e33c98b7348
IanDarwin <>
2006-09-26 04:39:11 +0000

    The solutions folder.

---
+++ solutions/sol21/Fred.java
@@ -0,0 +1,11 @@
+package ex21;
+
+import java.util.Date;
+
+public class Fred {
+
+	public Object getValue() {
+		return new Date();
+	}
+
+}
---
+++ solutions/sol21/FredTest.java
@@ -0,0 +1,16 @@
+package ex21;
+
+
+import junit.framework.TestCase;
+
+public class FredTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testOne() throws Exception {
+		Fred f = new Fred();
+		assertNotNull(f.getValue());
+	}
+}
