4541af4fabf9aa605d3a2e46ae9ff90d3c7d47e4
IanDarwin <>
2008-06-02 23:56:26 +0000

    Four-ify.

--- src/intro/TestString.java
+++ src/intro/TestString.java
@@ -1,12 +1,16 @@
 package intro;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 /**
  * One or two simple tests on java.lang.String, just so
  * I can build a TestSuite in this directory :-)
  */
-public class TestString extends TestCase {
+public class TestString {
+	
+	@Test
 	public void testStringLength() {
 		assertEquals("empty string length", 0, "".length());
 		assertEquals("1-char string", 1, "X".length());
