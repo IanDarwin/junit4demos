ed34a3dc4faf19aefbd4150ba43d0efead285671
IanDarwin <>
2008-06-04 20:27:26 +0000

    Update list of classes.

--- src/intro/AllTests.java
+++ src/intro/AllTests.java
@@ -6,8 +6,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/**
+ * X is a Y.
+ */
 @RunWith(value=Suite.class)
-@SuiteClasses(value={FooTest.class, TestString.class})
+@SuiteClasses(value={FooTest.class, FooTest2.class,
+			SetupVsConstrTest.class, TestString.class,
+			// "WhatThrows" is not a real test, and always fails.
+			})
 public class AllTests extends TestCase {
 	// No code needed!
 }
