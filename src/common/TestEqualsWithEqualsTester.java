90da1aa1093ec468bbd04ac3d0a86e391a4c7db3
IanDarwin <>
2008-06-15 00:20:14 +0000

    Remove useless printouts.

--- src/common/TestEqualsWithEqualsTester.java
+++ src/common/TestEqualsWithEqualsTester.java
@@ -10,13 +10,11 @@ public class TestEqualsWithEqualsTester  {
 
 	@Test public void testEquals() {
 		Foo f = new Foo();	// original
-		System.out.println(f);
 		Foo g = new Foo();	// equal
 		Foo h = new Foo(55); //notequal
 		Foo i = new Foo() {	// subclass
 		};
-		System.out.println(i);
-
+		
 		new EqualsTester(f, g, h, i);
 	}
 }
