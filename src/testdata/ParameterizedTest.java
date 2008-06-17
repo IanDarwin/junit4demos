328f9a05f07f727ac4d4c049e3c0b1ed176ab4fb
IanDarwin <>
2008-06-17 18:04:12 +0000

    Remove dead body. Add Parameterized Runner demo. Fourify AllTests.

---
+++ src/testdata/ParameterizedTest.java
@@ -0,0 +1,43 @@
+package testdata;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/*
+ * This is a JUnit test for the StringUtils class.  
+ */
+@RunWith(value=Parameterized.class)
+public class ParameterizedTest {
+	
+	/** This method provides data to the constructor for use in tests */
+	@Parameters
+	public static List<Object[]> data() {		
+		final Object[][] data = new Object[][]  {
+				{true,  ""},
+				{false, "null"},
+				{true,  null},
+		};
+		return Arrays.asList(data);					
+	}
+	
+	String s;
+	boolean expected;
+	
+	/** Constructor, gets arguments from data array; cast as needed */
+	public ParameterizedTest(Object expected, Object value) {
+		s = (String)value;
+		this.expected = (Boolean) expected;
+	}
+	
+	@Test
+	public void stringLengthTest() {
+		assert (s.length() == 0) == expected;	
+	}		
+}
+
+
