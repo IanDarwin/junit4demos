c0f1510954bf8a6c3ac3f359c22831c2e9f52a69
IanDarwin <>
2008-06-17 20:07:23 +0000

    Fourify; Go Generic.

--- src/mocking/TestSearchWithCrashTestDummy.java
+++ src/mocking/TestSearchWithCrashTestDummy.java
@@ -2,24 +2,21 @@ package mocking;
 
 import java.util.List;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class TestSearchWithCrashTestDummy extends TestCase {
+public class TestSearchWithCrashTestDummy {
 	public interface Search {
-		List search(int category);
+		List<Object> search(int category);
 	}
 	public class CrashTestDummySearch implements Search {
-		public List search(int category) {
+		public List<Object> search(int category) {
 			throw new NoProductFoundException();
 		}
 	}
+	
+	@Test(expected=NoProductFoundException.class)
 	public void testSearcherThrowsExceptionWithNoProducts() {
-		try {
-			new SearchEngine(new CrashTestDummySearch()).run();
-			fail("SearchEngine failed to throw NPFE");
-		} catch (NoProductFoundException e) {
-			// all is well
-		}
+		new SearchEngine(new CrashTestDummySearch()).run();
 	}
 
 	// These would already exist and are just to make it compile
