1a0040cd7647f94c3cde9067f6737189bbad22ce
IanDarwin <>
2008-06-17 19:42:18 +0000

    Top-level AllTests, must manage manually.

---
+++ src/AllAllTests.java
@@ -0,0 +1,15 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(value=Suite.class)
+@SuiteClasses(value={
+		intro.AllTests.class,
+		common.AllTests.class,
+		testdata.AllTests.class,
+		xml.AllTests.class,
+})
+public class AllAllTests {
+
+}
